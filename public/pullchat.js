794b3b334556bb16e8fd1a6dcf5bbc7e791c1aa1
Hannes Steffenhagen <cubicentertain@gmail.com>
2014-01-25 17:34:24 +0100

    Fixed scrolling problems, enter does now submit

--- public/pullchat.js
+++ public/pullchat.js
@@ -1,18 +1,32 @@
 //reload chat every 5 seconds, and when a message was posted by ourselves
 $(function() {
-    function reloadChat() {
-        $('#chatbox').load('/ #chatbox>li',function() {})
+    var reloadChat = function(alwaysScroll) {
+        var chatbox = $('#chatbox')
+        //scroll down after loading if we're told to do so or if the chatbox was completely scrolled down already
+        var isScrolledDown = chatbox.prop('scrollHeight') - chatbox.scrollTop() == chatbox.outerHeight()
+        var scrollAfterDone = alwaysScroll || isScrolledDown
+        chatbox.load('/ #chatbox>li',function() {
+            if(scrollAfterDone) {
+                chatbox.scrollTop(chatbox.prop('scrollHeight'))
+            }
+        })
+        
     }
-    window.setInterval(reloadChat,5000)
-    $("#post_button").click(function() {
+    window.setInterval(function(){reloadChat(false)},5000)
+    $("#message_form").submit(function() {
         $.ajax({
             type: 'POST',
             url: '/post_message',
             data: $("#message").val(),
             success: function(_data) {
-                reloadChat()
+                reloadChat(true)
             }
         })
+        //don't reload page - causes
+        //warnings in recent chrome versions.
+        //This appears to be caused by jQuery internals
+        //and isn't likely to cause any actual problems now or in the future
+        return false
     })
 })
 
